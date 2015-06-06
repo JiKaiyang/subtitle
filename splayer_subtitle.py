72635be8b28200cbcbe5b324a054e075326c5fbe
jikaiyang <jky.yang@gmail.com>
2015-06-06 18:38:06 +0900

    fix misspelled words

--- splayer_subtitle.py
+++ splayer_subtitle.py
@@ -26,7 +26,7 @@ def hash_video(filename):
             res = ";".join(map(lambda x:video_md5(video, x, block), offset))
     return res
 
-class Splayer_subtle(object):
+class Splayer_subtitle(object):
     __url = "https://www.shooter.cn/api/subapi.php"
     __path = "/tmp"
     def __init__(self, url = None, path = None, format = "json", lang = "Chn", hash_func = hash_video):
@@ -34,7 +34,7 @@ class Splayer_subtle(object):
         self.path = self.__path if path is None else path
         self.hash_func = hash_func
         self.filename = None
-        self.subtles = []
+        self.subtitles = []
         self.params = {"filehash": None, "pathinfo": None, "format": format, "lang":lang}
 
     def setfile(self, filename):
@@ -53,41 +53,41 @@ class Splayer_subtle(object):
             print "Warning:: Please set correct filename"
             sys.exit(1)
 
-    def fetch_subtle_list(self):
-        self.subtles = json.loads(urlopen(self.request).readlines()[0])
+    def fetch_subtitle_list(self):
+        self.subtitles = json.loads(urlopen(self.request).readlines()[0])
 
-    def fetch_subtle_cand(self, index = 0):
-        if len(self.subtles) <= index:
-            print "No subtle candidate"
+    def fetch_subtitle_cand(self, index = 0):
+        if len(self.subtitles) <= index:
+            print "No subtitle candidate"
             sys.exit(2)
-        subtle = self.subtles[index]
-        if len(subtle[u'Files']) > 1:
+        subtitle = self.subtitles[index]
+        if len(subtitle[u'Files']) > 1:
             print "Not supported yet"
             sys.exit(3)
         subinfo = {}
         for i in ['Desc', 'Delay']:
-            subinfo[i] = subtle[i]
-        subinfo['filename'] = os.path.join(self.path, self.params['pathinfo'] + "." + subtle['Files'][0]['Ext'])
+            subinfo[i] = subtitle[i]
+        subinfo['filename'] = os.path.join(self.path, self.params['pathinfo'] + "." + subtitle['Files'][0]['Ext'])
         try:
             print "try fetching Subtle file to "+subinfo['filename']
-            print 'target url:: ' + subtle['Files'][0]['Link']
-            urlretrieve(subtle['Files'][0]['Link'], subinfo['filename'])
+            print 'target url:: ' + subtitle['Files'][0]['Link']
+            urlretrieve(subtitle['Files'][0]['Link'], subinfo['filename'])
         except Exception as e:
             print e
             sys.exit(4)
-        print "Successfully downloaded subtle file: " + subinfo['filename']
+        print "Successfully downloaded subtitle file: " + subinfo['filename']
         return subinfo
 
-def fetch_subtle(filename, index = 0):
-    splayer_subtle = Splayer_subtle()
-    splayer_subtle.setfile(filename)
-    splayer_subtle.make_request()
-    splayer_subtle.fetch_subtle_list()
-    return splayer_subtle.fetch_subtle_cand(index)
+def fetch_subtitle(filename, index = 0):
+    splayer_subtitle = Splayer_subtitle()
+    splayer_subtitle.setfile(filename)
+    splayer_subtitle.make_request()
+    splayer_subtitle.fetch_subtitle_list()
+    return splayer_subtitle.fetch_subtitle_cand(index)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print "Please provide at least 1 video file"
         sys.exit(6)
     for i in range(1, len(sys.argv)):
-        print fetch_subtle(sys.argv[i])
+        print fetch_subtitle(sys.argv[i])
